65f6de41d0717fed3f897b9077b8d44ffe770386
youngyin <57608725+youngyin@users.noreply.github.com>
2025-06-29 03:31:35 +0900

    [feat] delete backup

--- module-user/temp_backup/UserXpHistoryRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.teamfair.moduleuser.adapter.out.persistence.repository
-
-import com.teamfair.moduleuser.adapter.out.persistence.entity.UserXpHistoryEntity
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface UserXpHistoryRepository : JpaRepository<UserXpHistoryEntity, Long> {
-    fun findByUserEntityId(userId: Long): List<UserXpHistoryEntity>
-} 
--- module-user/temp_backup/UserXpRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.teamfair.moduleuser.adapter.out.persistence.repository
-
-import com.teamfair.moduleuser.adapter.out.persistence.entity.UserXpEntity
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface UserXpRepository : JpaRepository<UserXpEntity, Long> {
-    fun findByUserEntityId(userId: Long): List<UserXpEntity>
-} 
