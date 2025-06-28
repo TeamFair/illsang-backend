63917fb733a3583e4e2efbdfab25d09822d3fade
youngyin <57608725+youngyin@users.noreply.github.com>
2025-06-29 03:06:37 +0900

    [feat] quest repository

--- module-quest/src/main/kotlin/com/teamfair/modulequest/adapter/out/persistence/repository/MissionRepository.kt
+++ module-quest/src/main/kotlin/com/teamfair/modulequest/adapter/out/persistence/repository/MissionRepository.kt
@@ -3,6 +3,6 @@ package com.teamfair.modulequest.adapter.out.persistence.repository
 import com.teamfair.modulequest.adapter.out.persistence.entity.MissionEntity
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface MissionRepository : JpaRepository<MissionEntity, Long>{
-
+interface MissionRepository : JpaRepository<MissionEntity, Long> {
+    fun findByQuestId(questId: Long): List<MissionEntity>
 }
--- module-quest/src/main/kotlin/com/teamfair/modulequest/adapter/out/persistence/repository/QuestRepository.kt
+++ module-quest/src/main/kotlin/com/teamfair/modulequest/adapter/out/persistence/repository/QuestRepository.kt
@@ -1,8 +1,9 @@
 package com.teamfair.modulequest.adapter.out.persistence.repository
 
-import com.teamfair.modulequest.adapter.out.persistence.entity.QuizAnswerEntity
+import com.teamfair.modulequest.adapter.out.persistence.entity.QuestEntity
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface QuestRepository : JpaRepository<com.teamfair.modulequest.adapter.out.persistence.entity.QuestEntity, Long> {
-
+interface QuestRepository : JpaRepository<QuestEntity, Long> {
+    fun findByType(type: String): List<QuestEntity>
+    fun findByPopularYn(popularYn: Boolean): List<QuestEntity>
 }
