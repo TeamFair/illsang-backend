4fe9901940e7aa910727e82a93615d694370e595
wonjongkimkim <dnjswhd0727@daum.net>
2025-08-24 00:35:47 +0900

    [fix] challenge/랜덤퀴즈 sql 문법오류

--- module-quest/src/main/kotlin/com/illsang/quest/repository/quest/QuizRepository.kt
+++ module-quest/src/main/kotlin/com/illsang/quest/repository/quest/QuizRepository.kt
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 
 interface QuizRepository : JpaRepository<QuizEntity, Long> {
-    @Query(value = "SELECT q FROM QuizEntity q WHERE q.mission.id = :missionId ORDER BY RAND() LIMIT 1")
+    @Query(value = "SELECT q FROM QuizEntity q WHERE q.mission.id = :missionId ORDER BY RANDOM() LIMIT 1")
     fun findRandomByMissionId(missionId: Long): QuizEntity?
 }
